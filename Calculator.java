610140196b3610705692304af3d672c0e8a5b97b
abhilashbhowmik <abhilash.bhowmik18@st.niituniversity.in>
2020-09-04 13:40:00 +0530

    Added subtraction and multiplication segment to the menu driven program

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Calculator {
+    private static Scanner input;
     public static void divide(float a, float b)
     {   if (b >0)
         {
@@ -15,9 +16,18 @@ public class Calculator {
         System.out.println("result of addition " + c);
     }
 
+    public static void subtraction(float a, float b) {
+        float c = a - b;
+        System.out.println("result of subtraction " + c);
+    }
+
+    public static void multiplication(float a, float b) {
+        float c = a * b;
+        System.out.println("result of multiplication " + c);
+    }
     public static void main (String[] args)
     {
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(System.in);
         System.out.print("enter 1st num : ");
         float a = input.nextFloat();
 
@@ -25,7 +35,9 @@ public class Calculator {
         float b = input.nextFloat();
 
         divide(a,b);
-        addition(a,b);
+        addition(a, b);
+        subtraction(a, b);
+        multiplication(a, b);
 
     }
 
